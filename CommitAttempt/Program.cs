c479b5b1bd9b9d1de15319ba883d6a091691f579
sabina <sabinav23@gmail.com>
2020-11-20 15:18:39 +0200

    Change message

--- CommitAttempt/Program.cs
+++ CommitAttempt/Program.cs
@@ -6,7 +6,7 @@ namespace CommitAttempt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("SUCCES");
         }
     }
 }
