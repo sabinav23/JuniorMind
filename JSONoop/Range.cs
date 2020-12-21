7cc64d0b603aa45a50f1c874cb58041f7c30f7ec
sabina <sabinav23@gmail.com>
2020-12-21 11:30:24 +0200

    add JSON OOP

---
+++ JSONoop/Range.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONoop
+{
+    public class Range
+    {
+        private char start;
+        private char end;
+
+        public Range(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] < start || text[i] > end)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ JSONoop/RangeFacts.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using JSONoop;
+
+namespace JSONoop
+{
+    public class RangeFacts
+    {
+        [Fact]
+        public void ReturnsTrueWhenInValidRange()
+        {
+            Range range1 = new Range('a', 'f');
+
+            string test = "abc";
+
+            Assert.True(range1.Match(test));
+        }
+
+        [Fact]
+        public void ReturnsTrueWhenCharacterIsBoundary()
+        {
+            Range range1 = new Range('a', 'f');
+
+            string test = "fda";
+
+            Assert.True(range1.Match(test));
+        }
+
+        [Fact]
+        public void ReturnsFalseWhenExceedingBoundary()
+        {
+            Range range1 = new Range('a', 'f');
+
+            string test = "1da";
+
+            Assert.False(range1.Match(test));
+        }
+
+        [Fact]
+        public void ReturnsFalseWhenNull()
+        {
+            Range range1 = new Range('a', 'f');
+
+            string test = null;
+
+            Assert.False(range1.Match(test));
+        }
+
+        [Fact]
+        public void ReturnsFalseWhenEmpty()
+        {
+            Range range1 = new Range('a', 'f');
+
+            string test = "";
+
+            Assert.False(range1.Match(test));
+        }
+    }
+}
