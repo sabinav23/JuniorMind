21b7ec933447d24f3e1a8dcc2d59d1cf0be580e8
sabina <sabinav23@gmail.com>
2020-12-24 11:05:27 +0200

    Add class Any and AnyFacts

---
+++ JSON_OOP/Any.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONoop
+{
+    class Any : IPattern
+    {
+        private string accepted;
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+        public IMatch Match(string text)
+        {
+            return !string.IsNullOrEmpty(text) && accepted.Contains(text[0])
+                ? new Match(text.Substring(1), true)
+                : new Match(text, false);
+        }
+    }
+}
---
+++ JSON_OOP/AnyFacts.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JSONoop
+{
+    public class AnyFacts
+    {
+        [Fact]
+        public void VerifyFunctionReturnsRemainingTextAndTrueWhenInRange()
+        {
+            var e = new Any("eE");
+
+            string test = "ea";
+
+            Match match = new Match("a", true);
+
+            Assert.True(match.Success().Equals((e.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((e.Match(test).RemainingText())));
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsRemainingTextAndTrueWhenSearchedCharIsSecond()
+        {
+            var e = new Any("eE");
+
+            string test = "Ea";
+
+            Match match = new Match("a", true);
+
+            Assert.True(match.Success().Equals((e.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((e.Match(test).RemainingText())));
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsFullTextAndFalseWhenCharNotInString()
+        {
+            var e = new Any("eE");
+
+            string test = "a";
+
+            Match match = new Match("a", false);
+
+            Assert.True(match.Success().Equals((e.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((e.Match(test).RemainingText())));
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsFullTextAndFalseWhenNull()
+        {
+            var e = new Any("eE");
+
+            string test = null;
+
+            Match match = new Match(null, false);
+
+            Assert.True(match.Success() == e.Match(test).Success());
+            Assert.True(match.RemainingText() == e.Match(test).RemainingText());
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsFullTextAndFalseWhenEmpty()
+        {
+            var e = new Any("eE");
+
+            string test = "";
+
+            Match match = new Match("", false);
+
+            Assert.True(match.Success().Equals((e.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((e.Match(test).RemainingText())));
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsRemainingTextAndTrueWhenSingInString()
+        {
+            var e = new Any("-+");
+
+            string test = "+3";
+
+            Match match = new Match("3", true);
+
+            Assert.True(match.Success().Equals((e.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((e.Match(test).RemainingText())));
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsRemainingTextAndTrueWhenSingIsFirstInString()
+        {
+            var e = new Any("-+");
+
+            string test = "-2";
+
+            Match match = new Match("2", true);
+
+            Assert.True(match.Success().Equals((e.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((e.Match(test).RemainingText())));
+        }
+
+    }
+}
