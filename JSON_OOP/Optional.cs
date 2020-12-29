a8e74fc5774a3d954b71b68e353c83ca1b2ab171
sabina <sabinav23@gmail.com>
2020-12-29 10:08:57 +0200

    Add Optional and OptionalFacts

---
+++ JSON_OOP/Optional.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONoop
+{
+    class Optional : IPattern
+    {
+        private IPattern pattern;
+
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return new Match(pattern.Match(text).RemainingText(), true);
+        }
+    }
+}
---
+++ JSON_OOP/OptionalFacts.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JSONoop
+{
+    public class OptionalFacts
+    {
+        [Fact]
+        public void VerifyFunctionReturnsRemainingTextAndTrueIfHasChar()
+        {
+            var a = new Optional(new Character('a'));
+
+            string test = "abc";
+
+            Match match = new Match("bc", true);
+
+            Assert.True(match.Success().Equals((a.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((a.Match(test).RemainingText())));
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsRemainingTextAndTrueIfHasMultipleChars()
+        {
+            var a = new Optional(new Character('a'));
+
+            string test = "aabc";
+
+            Match match = new Match("abc", true);
+
+            Assert.True(match.Success().Equals((a.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((a.Match(test).RemainingText())));
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsRemainingTextAndTrueIfHasNoCharMatching()
+        {
+            var a = new Optional(new Character('a'));
+
+            string test = "bc";
+
+            Match match = new Match("bc", true);
+
+            Assert.True(match.Success().Equals((a.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((a.Match(test).RemainingText())));
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsRemainingTextAndTrueIfEmpty()
+        {
+            var a = new Optional(new Character('a'));
+
+            string test = "";
+
+            Match match = new Match("", true);
+
+            Assert.True(match.Success().Equals((a.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((a.Match(test).RemainingText())));
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsRemainingTextAndTrueIfNull()
+        {
+            var a = new Optional(new Character('a'));
+
+            string test = null;
+
+            Match match = new Match(null, true);
+
+            Assert.True(match.Success() == a.Match(test).Success());
+            Assert.True(match.RemainingText() == a.Match(test).RemainingText());
+        }
+
+        [Fact]
+        public void VerifyFunctionReturnsRemainingTextAndTrueWhenCharIsSign()
+        {
+            var a = new Optional(new Character('-'));
+
+            string test = "-aabc";
+
+            Match match = new Match("aabc", true);
+
+            Assert.True(match.Success().Equals((a.Match(test).Success())));
+            Assert.True(match.RemainingText().Equals((a.Match(test).RemainingText())));
+        }
+    }
+}
