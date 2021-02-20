47c36521215e4862f1c977bf702ef7d81986b3d1
sabina <sabinav23@gmail.com>
2021-02-20 15:50:49 +0200

    add final Validation

--- JSON_OOP/JSON.cs
+++ JSON_OOP/JSON.cs
@@ -1,11 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace JSONoop
 {
     class JSON
     {
-     //   static void Main+90
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || (string.IsNullOrEmpty(args[0]) && !File.Exists(args[0])))
+            {
+                Console.WriteLine("You must insert the path to an existing file.");
+                return;
+            }
+
+            string path = args[0];
+            Value val = new Value();
+            string text = System.IO.File.ReadAllText(path);
+            var match = val.Match(text);
+
+            if (match.RemainingText() == "" && match.Success())
+            {
+                Console.WriteLine("Text is a valid JSON");
+            }
+            else
+            {
+                Console.WriteLine("Text is not a valid JSON");
+            }
+
+        }
     }
 }
