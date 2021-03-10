901ca86a9d87caea5a5322a8cbe7b03aa5ea8f9b
sabina <sabinav23@gmail.com>
2021-03-10 09:17:07 +0200

    Add Exceptions examples

---
+++ DataStructures/ExceptionExamples.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntArrayProject
+{
+    class ExceptionExamples
+    {
+        int[] arr = new int[5] { 1, 2, 3, 4, -15 };
+        string[] nume = new string[5] { "Mos", "Craciun", "Iepurasul", "Spiridusul", null };
+
+        public int CountSumOfElements()
+        {
+            int count = 0;
+            try
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    count += arr[i];
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                count = -1;
+            }
+
+            return count;
+        }
+
+        public int CountSumOfElementsMultimpleCatch()
+        {
+            int count = 0;
+            try
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    count /= i - 5;
+                }
+
+                return count = 10;
+                
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return count = 200;
+        }
+
+        public string FormWord()
+        {
+            string cuvant = "";
+            try
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    cuvant = nume[i].Substring(2);
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                cuvant = e.Message;
+            }
+
+            return cuvant;
+        }
+        public string GetFirstLetterOfAWord(string cuvant)
+        {
+            if (cuvant == null)
+            {
+                throw new ArgumentNullException();
+            }
+            return cuvant.Substring(0, 1);
+        }
+    }
+}
---
+++ DataStructures/ExceptionsFacts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IntArrayProject
+{
+    public class ExceptionsFacts
+    {
+
+        [Fact]
+        public void ReturnsExceptionMessage()
+        {
+            var ex = new ExceptionExamples();
+
+            Assert.Equal(-1, ex.CountSumOfElements());
+        }
+
+        [Fact]
+        public void ReturnsOutsideOfTryCount()
+        {
+            var ex = new ExceptionExamples();
+
+            Assert.Equal(200, ex.CountSumOfElementsMultimpleCatch());
+        }
+
+        [Fact]
+        public void ReturnErrorMessage()
+        {
+            var ex = new ExceptionExamples();
+
+            Assert.Equal("Object reference not set to an instance of an object.", ex.FormWord());
+        }
+
+        [Fact]
+        public void FunctionThrowsCorrectExceptionForCase()
+        {
+            var ex = new ExceptionExamples();
+
+            Exception exception = Assert.Throws<ArgumentNullException>(() => ex.GetFirstLetterOfAWord(null));
+
+            Assert.Equal("Value cannot be null.", exception.Message);
+        }
+    }
+}
